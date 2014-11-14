3c4871b83bbf4c267eba148ee09b634091131772
Le Van Nghia <levan_nghia@cyberagent.co.jp>
2014-11-14 19:10:31 +0900

    Initial Commit

---
+++ MaterialKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaterialKit
+//
+//  Created by LeVan Nghia on 11/14/14.
+//  Copyright (c) 2014 Le Van Nghia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
