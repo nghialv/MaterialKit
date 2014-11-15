5f5450b58c7e21fb1bc0dcdae970547c40347861
Le Van Nghia <nghialv2607@gmail.com>
2014-11-15 22:02:41 +0900

    add textfieldviewcontroller

--- Example/MaterialKit/TextFieldViewController.swift
+++ Example/MaterialKit/TextFieldViewController.swift
@@ -1,13 +1,14 @@
 //
-//  ViewController.swift
+//  TextFieldViewController.swift
 //  MaterialKit
 //
 //  Created by Le Van Nghia on 11/15/14.
 //  Copyright (c) 2014 Le Van Nghia. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class TextFieldViewController: UIViewController {
+    
 }
