862489aa7a92d1108d628736be9c8f4be5ef8ff4
Leo Lou <louyuhong@gmail.com>
2015-03-05 13:47:04 +0800

    Add xcode project

---
+++ MaterialKit/MaterialKit.h
@@ -0,0 +1,19 @@
+//
+//  MaterialKit.h
+//  MaterialKit
+//
+//  Created by Leo on 3/5/15.
+//  Copyright (c) 2015 MaterialKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialKit.
+FOUNDATION_EXPORT double MaterialKitVersionNumber;
+
+//! Project version string for MaterialKit.
+FOUNDATION_EXPORT const unsigned char MaterialKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialKit/PublicHeader.h>
+
+
