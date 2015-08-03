20fb5c6221771fe5615c2f7e7f0df5e8370a5e6e
Matthew Clarkson <mpclarkson@users.noreply.github.com>
2015-08-03 13:49:05 +1000

    Failable init

--- Source/MKLabel.swift
+++ Source/MKLabel.swift
@@ -60,7 +60,7 @@ public class MKLabel: UILabel {
     }
     private lazy var mkLayer: MKLayer = MKLayer(superLayer: self.layer)
 
-    required public init(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
