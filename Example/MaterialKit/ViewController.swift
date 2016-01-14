b550844eefa604543eb5da2d0d31c073dc043e5f
rahuliyer95 <rahuliyer573@gmail.com>
2016-01-14 19:47:10 +0530

    Added MKSnackbar

--- Example/MaterialKit/ViewController.swift
+++ Example/MaterialKit/ViewController.swift
@@ -9,5 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func cardViewClicked(sender: AnyObject) {
+        let snackbar = MKSnackbar(
+            withTitle: "You clicked on CardView\nThis is a SnackBar :)",
+            withDuration: nil,
+            withTitleColor: nil,
+            withActionButtonTitle: "Done",
+            withActionButtonColor: UIColor.MKColor.Blue)
+        snackbar.show()
+    }
 }
